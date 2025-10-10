98b2a3d98c727f9c0825e0c66d6d4daa822288b4
João Santos <santos_paulo17@hotmail.com>
2025-10-10 16:07:52 +0100

    Update layer_init.sql

--- scripts/bronze/layer_init.sql
+++ scripts/bronze/layer_init.sql
@@ -45,31 +45,31 @@ CREATE TABLE bronze.crm_prd_info(
 );
 
 CREATE TABLE bronze.crm_sales_details(
-  sls_ord_num VARCHAR(50),
-  sls_prd_key VARCHAR(50),
-  sls_cust_id INT,
-  sls_order_dt INT,
-  sls_ship_dt INT,
-  sls_due_dt INT,
-  sls_sales INT,
-  sls_quantity INT,
-  sls_price INT
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
 );
 
 CREATE TABLE bronze.erp_cust_az12(
-  cid VARCHAR(50),
-  bdate VARCHAR(10),
-  gen VARCHAR(10)
+	cid VARCHAR(50),
+	bdate VARCHAR(10),
+	gen VARCHAR(10)
 );
 
 CREATE TABLE bronze.erp_loc_a101(
-  cid VARCHAR(50),
-  cntry VARCHAR(50)
+	cid VARCHAR(50),
+	cntry VARCHAR(50)
 );
 
 CREATE TABLE bronze.erp_px_cat_g1v2(
-  id VARCHAR(50),
-  cat VARCHAR(50),
-  subcat VARCHAR(50),
-  maintenance VARCHAR(3)
+	id VARCHAR(50),
+	cat VARCHAR(50),
+	subcat VARCHAR(50),
+	maintenance VARCHAR(3)
 );
